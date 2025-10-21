4b0935244b73e0dfb4e544cdaca889c5c258be1e
Ludmila <ludmillalare53@gmail.com>
2025-10-21 11:26:36 +0200

    another step

--- users/views.py
+++ users/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
-
-# Create your views here.
-from rest_framework import generics, permissions
+from rest_framework import generics, permissions, status
+from rest_framework.response import Response
 from django.contrib.auth.models import User
 from .serializers import RegisterSerializer, UserSerializer
 
@@ -11,11 +10,17 @@ class RegisterView(generics.CreateAPIView):
     permission_classes = (permissions.AllowAny,)  # Public
     serializer_class = RegisterSerializer
 
-# Endpoint pour voir ou modifier son profil
-class UserDetailView(generics.RetrieveUpdateAPIView):
+# Endpoint pour voir, modifier ou supprimer son profil
+class UserDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = User.objects.all()
     permission_classes = (permissions.IsAuthenticated,)  # Auth uniquement
     serializer_class = UserSerializer
 
     def get_object(self):
+        # On récupère uniquement l'utilisateur connecté
         return self.request.user
+
+    def delete(self, request, *args, **kwargs):
+        user = self.get_object()
+        user.delete()
+        return Response({"detail": "Compte supprimé avec succès."}, status=status.HTTP_204_NO_CONTENT)
