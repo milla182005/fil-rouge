da5ccae37651ffd330ad25f33e8189ada4eab685
Ludmila <ludmillalare53@gmail.com>
2025-10-21 03:13:19 -0100

    another step

--- users/views.py
+++ users/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import generics, permissions
+from django.contrib.auth.models import User
+from .serializers import RegisterSerializer, UserSerializer
+
+# Endpoint pour s’inscrire
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    permission_classes = (permissions.AllowAny,)  # Public
+    serializer_class = RegisterSerializer
+
+# Endpoint pour voir ou modifier son profil
+class UserDetailView(generics.RetrieveUpdateAPIView):
+    queryset = User.objects.all()
+    permission_classes = (permissions.IsAuthenticated,)  # Auth uniquement
+    serializer_class = UserSerializer
+
+    def get_object(self):
+        return self.request.user
