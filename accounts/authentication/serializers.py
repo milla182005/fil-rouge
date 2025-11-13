9db1a6c120e0425fd4da5093daf411432475f7aa
Ludmila <ludmillalare53@gmail.com>
2025-11-13 18:54:21 +0100

    new step

--- accounts/authentication/serializers.py
+++ accounts/authentication/serializers.py
@@ -9,19 +9,22 @@ class RegisterSerializer(serializers.ModelSerializer):
         write_only=True, required=True, validators=[validate_password]
     )
     password2 = serializers.CharField(write_only=True, required=True)
-
+    
     class Meta:
-        ref_name = 'AuthenticationRegister'  # Nom unique pour Swagger
+        ref_name = 'AuthenticationRegister'
         model = User
         fields = ('username', 'email', 'password', 'password2')
         extra_kwargs = {'email': {'required': True}}
-
+    
     def validate(self, attrs):
         if attrs['password'] != attrs['password2']:
             raise serializers.ValidationError({"password": "Les mots de passe ne correspondent pas."})
         return attrs
-
+    
     def create(self, validated_data):
+        # IMPORTANT : Supprime password2
+        validated_data.pop('password2')
+        
         user = User.objects.create_user(
             username=validated_data['username'],
             email=validated_data['email'],
